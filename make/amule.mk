b0919a648701e04ca8c8c0e7a46728ba24ea03e3
Thomas Kupper <thomas.kupper@gmail.com>
2013-12-03 10:03:53 +0100

    amule: update to latest version 2.31

--- make/amule.mk
+++ make/amule.mk
@@ -22,7 +22,7 @@
 # http://developer.berlios.de/projects/amule/
 
 #AMULE_SITE=http://download.berlios.de/amule
-AMULE_VERSION=2.3.1rc1
+AMULE_VERSION=2.3.1
 #AMULE_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/amule
 AMULE_SITE=http://$(SOURCEFORGE_MIRROR)/project/amule/aMule/$(AMULE_VERSION)
 AMULE_SOURCE=aMule-$(AMULE_VERSION).tar.bz2
