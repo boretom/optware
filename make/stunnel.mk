e840cc608e4a0187faead6f8f7a0a535b5acd102
Thomas Kupper <thomas.kupper@gmail.com>
2013-12-07 00:54:01 +0100

    stunnel: update to latest version 4.56

--- make/stunnel.mk
+++ make/stunnel.mk
@@ -19,8 +19,8 @@
 #
 # You should change all these variables to suit your package.
 #
-STUNNEL_SITE=http://www.stunnel.org/download/stunnel/src
-STUNNEL_VERSION=4.26
+STUNNEL_SITE=http://www.stunnel.org/downloads
+STUNNEL_VERSION=4.56
 STUNNEL_SOURCE=stunnel-$(STUNNEL_VERSION).tar.gz
 STUNNEL_DIR=stunnel-$(STUNNEL_VERSION)
 STUNNEL_UNZIP=zcat
@@ -47,8 +47,8 @@ STUNNEL_CONFFILES=/opt/etc/stunnel/stunnel.conf \
 # STUNNEL_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-STUNNEL_PATCHES= \
-	$(STUNNEL_SOURCE_DIR)/configure.patch
+STUNNEL_PATCHES= 
+#	$(STUNNEL_SOURCE_DIR)/configure.patch
 
 #
 # If the compilation of the package requires additional
