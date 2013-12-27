bd218b7349bf92a109e5e893d1f4a7e194e4fb8b
Thomas Kupper <thomas.kupper@gmail.com>
2013-12-27 13:48:54 +0100

    libdvb: add missing <string.h> include

--- make/libdvb.mk
+++ make/libdvb.mk
@@ -56,7 +56,8 @@ LIBDVB_CONFFILES=
 # LIBDVB_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-LIBDVB_PATCHES=$(LIBDVB_SOURCE_DIR)/topf2ps.patch
+LIBDVB_PATCHES=$(LIBDVB_SOURCE_DIR)/topf2ps.patch \
+			$(LIBDVB_SOURCE_DIR)/cam_menu.hh.patch
 
 #
 # If the compilation of the package requires additional
